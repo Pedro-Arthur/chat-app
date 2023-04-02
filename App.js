e961d2eed9e54df142744d04dd2d4f7dc74689a0
Pedro Arthur <pedro13arthur@gmail.com>
2023-04-02 19:34:50 -0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Configuration, OpenAIApi } from 'openai';
+import 'react-native-url-polyfill/auto';
 
 export default function App() {
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  async function sendRequest() {
+    try {
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'Say about expo',
+        max_tokens: 7,
+        temperature: 0,
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={sendRequest} title="Send request" />
     </View>
   );
 }
